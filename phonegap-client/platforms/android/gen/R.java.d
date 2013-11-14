749590b8e90da133f38633daad86e36997a87352
Justin Wark <justinwark@gmail.com>
2013-11-14 17:13:20 +1100

    Set up client as Cordova/PhoneGap app.

---
+++ phonegap-client/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/gen/com/example/angular_directory/R.java \
+ : /Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/drawable/icon.png \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/drawable-hdpi/icon.png \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/drawable-ldpi/icon.png \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/drawable-mdpi/icon.png \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/values/strings.xml \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/res/xml/config.xml \
+/Users/justinw/projects/angular-directory/phonegap-client/platforms/android/bin/AndroidManifest.xml \
